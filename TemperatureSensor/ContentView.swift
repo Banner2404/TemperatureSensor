9f56a9e203bc17470c181ebdf73dcdf649a411d3
Eugene Sobol <esobol@insoftgroup.com>
2020-04-02 15:36:46 +0300

    Initial Commit

---
+++ TemperatureSensor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TemperatureSensor
+//
+//  Created by Евгений Соболь on 4/2/20.
+//  Copyright © 2020 Esobol. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
